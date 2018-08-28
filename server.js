8c05adf27051fa000792c8fce7cf4a3b44ace0cb
ajaysagi1 <42770952+ajaysagi1@users.noreply.github.com>
2018-08-28 21:14:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app=http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   //sets the right header and status code after edit 3
   res.end('Hello World\n');
-  //outputs string with line end symbol
+  //outputs string with line end symbol after edit 4
 }).listen(1337, "127.0.0.1");
 //sets port and IP address of the server
 console.log('Server running at http://127.0.0.1:1337/');
